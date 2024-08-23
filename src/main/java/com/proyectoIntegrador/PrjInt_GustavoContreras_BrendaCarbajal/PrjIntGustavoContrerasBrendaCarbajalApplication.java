5dd9ab3b70219b668de1be729408a26e66785f60
Gustavo Contreras <guta.contre@hotmail.com>
2024-08-22 22:57:12 -0500

    firts commit

---
+++ src/main/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/PrjIntGustavoContrerasBrendaCarbajalApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.PrjInt_GustavoContreras_BrendaCarbajal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjIntGustavoContrerasBrendaCarbajalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjIntGustavoContrerasBrendaCarbajalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/PrjIntGustavoContrerasBrendaCarbajalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.PrjInt_GustavoContreras_BrendaCarbajal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjIntGustavoContrerasBrendaCarbajalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
