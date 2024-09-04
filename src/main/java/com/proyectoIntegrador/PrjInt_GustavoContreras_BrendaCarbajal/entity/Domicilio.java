9a6008ee8c171798695629488bc92d0cba6a1bbd
Gustavo Contreras <guta.contre@hotmail.com>
2024-09-03 22:57:08 -0500

    fix: bucle postman metodo get ai insertar un paciente

--- src/main/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/entity/Domicilio.java
+++ src/main/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/entity/Domicilio.java
@@ -1,5 +1,6 @@
 package com.proyectoIntegrador.PrjInt_GustavoContreras_BrendaCarbajal.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,5 +24,6 @@ public class Domicilio {
 
     // Relación inversa con Paciente (opcional para evitar recursividad en JSON)
     @OneToOne(mappedBy = "domicilio")
+    @JsonIgnore
     private Paciente paciente;
 }
