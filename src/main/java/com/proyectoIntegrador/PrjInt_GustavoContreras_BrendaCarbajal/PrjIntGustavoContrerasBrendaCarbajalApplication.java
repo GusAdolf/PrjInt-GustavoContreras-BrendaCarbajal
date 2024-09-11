8c8d3c09d1d6523076d3f85c84727b1c38b823e7
Gustavo Contreras <gustavo.contreras@fisagrp.com>
2024-09-11 09:01:15 -0500

    creacion rama feature

--- src/main/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/PrjIntGustavoContrerasBrendaCarbajalApplication.java
+++ src/main/java/com/proyectoIntegrador/PrjInt_GustavoContreras_BrendaCarbajal/PrjIntGustavoContrerasBrendaCarbajalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrjIntGustavoContrerasBrendaCarbajalApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PrjIntGustavoContrerasBrendaCarbajalApplication.class, args);
 	}
